535fd688dbc3b1a8835ac4aad3663361da598757
timaxlax <167482512+timaxlax@users.noreply.github.com>
2024-07-14 07:21:07 +0300

    Update notification_manager.py

--- telegram-bot/bot/services/notification_manager.py
+++ telegram-bot/bot/services/notification_manager.py
@@ -6,7 +6,7 @@ class NotificationManager:
         self.bot = Bot(token=TELEGRAM_TOKEN)
 
     async def send_registration_complete(self, chat_id, car_type):
-        await self.bot.send_message(chat_id, f"Регистрация успешно пройдена. Вашему автомобилю присвоен тип {car_type}.")
+        await self.bot.send_message(chat_id, f" Вашему автомобилю присвоен тип {car_type}.")
 
     async def send_type_changed(self, chat_id, new_car_type):
         await self.bot.send_message(chat_id, f"Тип вашего авто изменен на {new_car_type}.")
